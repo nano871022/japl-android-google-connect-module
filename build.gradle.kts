dae9c4abe765c7db637aa6c6939dd057e2785e16
alejandro parra <alejandroparral@gmail.com>
2024-04-27 23:23:42 -0500

    release new settings
    added new option to add configuration to google drive connect project
    
    Signed-off-by: alejandro parra <alejandroparral@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
     implementation("com.google.android.gms:play-services-drive:17.0.0")
+    implementation("com.google.firebase:firebase-database-ktx:20.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- src/main/java/co/com/japl/connect/gdrive/drive/GetFilesFromFolderShared.kt
+++ src/main/java/co/com/japl/connect/gdrive/drive/GetFilesFromFolderShared.kt
@@ -2,6 +2,7 @@ package co.com.japl.connect.gdrive.drive
 
 import android.content.Context
 import android.util.Log
+import androidx.annotation.RawRes
 import co.com.japl.connect.gdrive.BuildConfig
 import co.com.japl.connect.gdrive.R
 import co.com.japl.connect.gdrive.model.DriveFile
@@ -9,6 +10,12 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
 import com.google.api.services.drive.Drive
 import com.google.api.services.drive.DriveScopes
 import com.google.api.services.drive.model.FileList
+import java.io.File
+import java.io.FileOutputStream
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZoneOffset
 import java.util.concurrent.CompletableFuture
 
 class GetFilesFromFolderShared(val context:Context) {
@@ -17,23 +24,53 @@ class GetFilesFromFolderShared(val context:Context) {
     private val FOLDER_ID = BuildConfig.FOLDER_ID
     private val URL_INITIAL = BuildConfig.URL_INITIAL
 
-    fun execute():List<DriveFile>?{
-        val response = CompletableFuture.supplyAsync(){ getFiles() }
+    fun execute(@RawRes resId:Int = R.raw.cralameda181_34c486bb5b56, folder:String = FOLDER_ID):List<DriveFile>?{
+        val response = CompletableFuture.supplyAsync(){ getFiles(resId,folder) }
         return response.get()
     }
 
-    private fun getFiles():List<DriveFile>?{
+    fun downloadFile(idFile:String,@RawRes resId:Int = R.raw.cralameda181_34c486bb5b56, folder:String = FOLDER_ID):File?{
+        val response = CompletableFuture.supplyAsync(){ getFiles(idFile,resId,folder) }
+        return response.get()
+    }
+
+    private fun getFiles(@RawRes resId: Int, folder:String):List<DriveFile>?{
         Log.d("TaskTest6","<<<=== START:TaskTest6#doInBackground read files  $APPLICATION_NAME $MIME_TYPE_IMAGE $FOLDER_ID $URL_INITIAL")
-        val credentials = getCredentials()
+        val credentials = getCredentials(resId)
         val drive = getDriveConnection(credentials)
-        val result = getListFiles(drive)
+        val result = getListFiles(drive,folder)
         return result?.files?.map {
-            DriveFile(it.id, it.name, it.mimeType, URL_INITIAL + it.id)
+            DriveFile(
+                id = it.id
+                , name = it.name
+                , mimeType =  it.mimeType
+                , url=URL_INITIAL + it.id
+                , date = LocalDateTime.ofInstant(Instant.ofEpochMilli(it.createdTime?.value?:LocalDateTime.now().toInstant(ZoneOffset.UTC).toEpochMilli()), ZoneId.systemDefault())
+                , version = it.version?.toString()?:"1"
+                , description = it.description)
         }?.also { Log.d("TaskTest6","<<<=== FINISH:TaskTest6#doInBackground read files $it $credentials $drive $result") }
     }
 
-    private fun getCredentials(): GoogleCredential? {
-        val stream = context.resources.openRawResource(R.raw.cralameda181_34c486bb5b56)
+    private fun getFiles(idFile:String,@RawRes resId: Int, folder:String):File?{
+        Log.d("TaskTest7","<<<=== START:TaskTest6#doInBackground read files  $APPLICATION_NAME $MIME_TYPE_IMAGE $FOLDER_ID $URL_INITIAL")
+        val credentials = getCredentials(resId)
+        val drive = getDriveConnection(credentials)
+        val fileFound = getFile(idFile,folder,drive) ?: return null
+
+        val file = File(context.filesDir, fileFound.name)
+        try {
+            val outputStream = FileOutputStream(file)
+            downloadFile(idFile,drive, outputStream)
+            outputStream.flush()
+            outputStream.close()
+        }catch (e:Exception){
+            Log.e("TaskTest7","<<<=== ERROR:TaskTest6#doInBackground read files $e")
+        }
+        return file?.also { Log.d("TaskTest7","<<<=== FINISH:TaskTest6#doInBackground read files $it $credentials $drive ${it.name}") }
+    }
+
+    private fun getCredentials(@RawRes resId: Int): GoogleCredential? {
+        val stream = context.resources.openRawResource(resId)
         return GoogleCredential.fromStream(stream)
             .createScoped(listOf(DriveScopes.DRIVE_READONLY))
     }
@@ -44,12 +81,27 @@ class GetFilesFromFolderShared(val context:Context) {
             .build()
     }
 
-    private fun getListFiles(drive:Drive?): FileList? {
+    private fun getFile(idFile:String,folder:String,drive:Drive?):com.google.api.services.drive.model.File? {
+        return getListFiles(folder,drive)?.files?.firstOrNull { it.id == idFile}
+    }
+
+    private fun getListFiles(drive:Drive?,folder:String,mimeType:String = MIME_TYPE_IMAGE): FileList? {
         return drive?.files()?.list()
-            ?.setQ(MIME_TYPE_IMAGE)
-            ?.setQ("'$FOLDER_ID' in parents")
-            ?.setFields("files(id, name, mimeType)")
+            ?.setQ(mimeType)
+            ?.setQ("'$folder' in parents")
+            ?.setFields("files(id, name, mimeType, version, createdTime, description)")
             ?.execute()
     }
 
+    private fun getListFiles(folder:String,drive:Drive?): FileList? {
+        return drive?.files()?.list()
+            ?.setQ("'$folder' in parents")
+            ?.setFields("files(id, name, mimeType, version, createdTime, description)")
+            ?.execute()
+    }
+
+    private fun downloadFile(idFile:String,drive:Drive?,fileOutputStream:FileOutputStream) {
+        if(drive == null) return
+         drive.files()[idFile]?.executeAndDownloadTo(fileOutputStream)
+    }
 }
